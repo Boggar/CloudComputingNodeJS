da9fd76ddc25c56d33bb51fb071538ee71b3cab3
Manuel <manuel.thamm@hs-weingarten.de>
2024-10-26 13:04:10 +0200

    all functionalities working

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+/*Server Setup*/ 
 const express = require("express");
 const app = express();
 const port = 2000;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+/*Data Handling*/
+const comments = [];
+
+app.post('/submit-comment', (req, res) => {
+  const { name, comment } = req.body;
+  comments.push({ name, comment });
+  res.redirect('/comment.html'); // Redirecting the User back to the comment form
+});
+
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
