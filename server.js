158fa7df9cb2d0cc8c0e44720bb5ec19580c18ca
k-acaroglu <acaroglugoktan@gmail.com>
2024-10-26 11:18:17 +0200

    Website with 3 pages

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 2000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
