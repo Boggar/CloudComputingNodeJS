3459f349b55ed486bbc67c0a6d75eb96b10b5d25
Manuel <manuel.thamm@hs-weingarten.de>
2024-10-26 11:00:22 +0200

    test

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 2000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log('Server running at http://localhost:$(port)');
+});
